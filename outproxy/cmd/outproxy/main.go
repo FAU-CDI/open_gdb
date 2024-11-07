ba3b8afe2956cde2edc2c0a3bb92d79e731472e7
Tom Wiesing <tkw01536@gmail.com>
2024-11-07 21:14:33 +0100

    cmd/outproxy: Use 'ENABLE_IPV6' to conditionally enable ipv6

--- outproxy/cmd/outproxy/main.go
+++ outproxy/cmd/outproxy/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 	"outproxy"
 	"time"
 )
@@ -15,6 +16,12 @@ func main() {
 		Lookup:  net.LookupIP,
 		Timeout: timeout,
 	}
+
+	// if v6 is enabled, skip filtering v6
+	if enableV6 {
+		options.Filter = outproxy.IsLocal
+	}
+
 	proxy := outproxy.MakeProxy(options)
 
 	listener, err := net.Listen("tcp", addr)
@@ -27,11 +34,13 @@ func main() {
 }
 
 var addr string
+var enableV6 bool
 var timeout time.Duration
 
 func init() {
 	defer flag.Parse()
 
 	flag.StringVar(&addr, "addr", ":8080", "address to listen on")
+	flag.BoolVar(&enableV6, "ipv6", os.Getenv("ENABLE_IPV6") != "", "enable ipv6 support (set ENABLE_IPV6 environment variable to enable)")
 	flag.DurationVar(&timeout, "timeout", time.Second, "timeout for net dial")
 }
